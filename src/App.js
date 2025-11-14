9dd2d76326f064cd5307f8499db1aff98e13e4f2
BHAGYASHREEKHEMSHETTY <bhagynelge@gmail.com>
2025-11-14 16:26:40 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from "react";
+const App=()=>{
+  const [data,setData]=useState([]);
+  const [currentPage,setCurrentPage]=useState(1);
+  const [isLoading,setIsLoading]=useState(true);
+  const [error,setError]=useState(null);
+  const itemsPerPage=10;
+  useEffect(()=>{
+    const fetchData=async()=>{
+      try{
+        const response=await fetch(
+          'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json'
+        );
+        if(!response.ok){
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setData(result);
+        setIsLoading(false);
+      } catch (error) {
+        setError('failed to fetch data');
+        setIsLoading(false);
+        alert("Failed to fetch data.Please try again later.");
+      }
+    };
+    fetchData();
+  },[]);
+  const handlePrevious=()=>{
+    if(currentPage>1){
+      setCurrentPage(currentPage-1);
+    }
+  };
+  const handleNext=()=>{
+    const totalPages=Math.ceil(data.length/itemsPerPage);
+    if(currentPage<totalPages){
+      setCurrentPage(currentPage+1);
+    }
+  };
+  const getPaginationData=()=>{
+    const startIndex=(currentPage-1)*itemsPerPage;
+    const endIndex=startIndex+itemsPerPage;
+    return data.slice(startIndex,endIndex);
+  };
+  if(isLoading){
+    return <div>Loading...</div>;
+  }
+  if(error){
+    return <div>{error}</div>;
+  }
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1 style={{textAlign:"center"}}>Employee Data</h1>
+      <table border="1" style={{borderCollapse:"collapse",width:"100%"}}>
+        <thead style={{textAlign:"left",backgroundColor:"green"}}>
+          <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {getPaginationData().map((employee)=>(
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.name}</td>
+              <td>{employee.email}</td>
+              <td>{employee.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div style={{marginTop:"10px",textAlign:"center"}}>
+        <button
+         onClick={handlePrevious}
+         disabled={currentPage===1}
+         style={{marginRight:"10px",backgroundColor:"green"}}
         >
-          Learn React
-        </a>
-      </header>
+          Previous
+        </button>
+        <span>{currentPage}</span>
+        <button
+          onClick={handleNext}
+          disabled={currentPage===Math.ceil(data.length/itemsPerPage)}
+          style={{marginLeft:"10px",backgroundColor:"green"}}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
